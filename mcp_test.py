6cab6e26af5d65fa2cec1d91bb1041b1a65275d3
tristanpadiou <padioutristan@gmail.com>
2025-03-29 15:01:07 -0400

    add files

---
+++ mcp_test.py
@@ -0,0 +1,30 @@
+from pydantic_ai import Agent
+from pydantic_ai.mcp import MCPServerStdio
+from pydantic_ai import RunContext
+from pydantic_ai.providers.google_gla import GoogleGLAProvider
+from pydantic_ai.models.gemini import GeminiModel
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+google_api_key=os.getenv('google_api_key')
+tavily_api_key=os.getenv('tavily_key')
+
+llm=GeminiModel('gemini-2.0-flash', provider=GoogleGLAProvider(api_key=google_api_key))
+
+server = MCPServerStdio('python', [ '-m','mcp_server_time',"--local-timezone=America/New_York"])  
+agent = Agent(llm, mcp_servers=[server])
+async def main():
+    async with agent.run_mcp_servers():
+        result = await agent.run('hello')
+        while True:
+            print(f'\n{result.data}')
+            user_input = input('Enter a prompt: ')
+            result = await agent.run(user_input)
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
